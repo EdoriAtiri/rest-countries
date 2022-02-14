61342ef990821318a73dd449f53ce48ef1566a60
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-02-14 15:00:56 +0100

    Revert "changed home route to '/'"
    This reverts commit 556b9d492067686270e4de2da2c5eb850c54d520.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { DetailsComponent } from './details/details.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'detail/:name', component: DetailsComponent },
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: ErrorPageComponent },
 ];
 
